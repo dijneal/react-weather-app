c3ea6eba8f190f17ceff81addce98c7ad52f0ace
dijneal <133080518+dijneal@users.noreply.github.com>
2023-09-04 19:52:23 -0700

    added GitHub url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather App </h1>
+      <footer>
+        This project was coded by DiJonnae and is {""}
+        <a
+          href="https://github.com/dijneal/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-souced
+        </a>
+      </footer>
     </div>
   );
 }
