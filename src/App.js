4a340c99453a1b7e4f57147f0175487c35eb3ba9
dijneal <133080518+dijneal@users.noreply.github.com>
2023-09-05 15:41:26 -0700

    removed Bootstrap via CSS link and added it via npm install

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <footer>
-        This project was coded by DiJonnae and is {""}
-        <a
-          href="https://github.com/dijneal/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-souced
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App </h1>
+        <footer>
+          This project was coded by DiJonnae and is {""}
+          <a
+            href="https://github.com/dijneal/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-souced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
